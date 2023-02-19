b02320c62715cf0ac8bd7a18544e68b50ebf710b
DimaInNature <dimainnature@yandex.ru>
2023-02-19 09:45:04 +0300

    feat: Add empty ContactMessagesController.

---
+++ src/VictoryRestaurant.Contact.Presentation/Controllers/ContactMessagesController.cs
@@ -0,0 +1,10 @@
+﻿namespace VictoryRestaurant.Contact.Presentation.Controllers;
+
+/// <summary> Controller for work with <see cref="ContactMessageEntity"/>'s. </summary>
+[Route(template: "api/contact-messages")]
+[Produces(contentType: "application/json")]
+[ApiController]
+public sealed class ContactMessagesController : ControllerBase
+{
+
+}
