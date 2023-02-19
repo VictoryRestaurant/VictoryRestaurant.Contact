8db9b9fa039fed4b4775a9734729136599b382d4
DimaInNature <dimainnature@yandex.ru>
2023-02-19 09:45:26 +0300

    feat: Add Presentation tier.

---
+++ src/VictoryRestaurant.Contact.Presentation/global.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.OpenApi.Models;
+global using Serilog;
+global using System.Reflection;
+global using System.Text.RegularExpressions;
+global using VictoryRestaurant.Contact.Presentation.Configurations;
